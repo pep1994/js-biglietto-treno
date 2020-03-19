351f177d6b5a4464510e2115b892f326aea322a5
Marco Petrini <marcopetrini1994@gmail.com>
2020-03-19 15:14:12 +0100

    logica tradotta

--- main.js
+++ main.js
@@ -1,22 +1,39 @@
 // calcolo prezzo biglietto treno
 
+
+// variabili
 var basePrice = 0.21;
 var userKilo;
 var userAge;
+var price;
+var discountAnderage;
+var discountOver65;
+var priceTot;
 
+
+// calcolo valore numerico sconto per minorenni
+discountAnderage = 20 / 100;
+
+// calcolo valore numerico sconto per over 65
+discountOver65 = 40 / 100;
 
 
 // chiediamo all'utente quanti kilometri deve percorrere
-userKilo = prompt("Quanti kilometri deve percorrere?");
+userKilo = parseInt(prompt("Quanti kilometri deve percorrere?"));
 
 // chiediamo all'utente la sua età
-userAge = prompt("Quanti anni ha?");
+userAge = parseInt(prompt("Quanti anni ha?"));
 
-// logica prezzo biglietto
-if (userAge < 18) {   // se l'utente ha meno di 18 anni
-  // allora il prezzo del biglietto avrà uno sconto del 20%
-} else if (userAge > 65) {  // altrimenti se l'utente avrà più di 65 anni
-  // allora il prezzo del biglietto avrà uno sconto del 40%
-} else { // altrimenti il prezzo del biglietto sarà quello base
+// calcoliamo il prezzo del biglietto senza lo sconto
+price = userKilo * basePrice;
 
+// logica
+if (userAge < 18) {
+  priceTot = price - (price * discountAnderage);
+} else if (userAge > 65) {
+  priceTot = price - (price * discountOver65);
+} else {
+  priceTot = price;
 }
+
+console.log(priceTot);
